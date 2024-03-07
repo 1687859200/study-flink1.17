3ba420de3cf2cbf018d506a99766eb98db0ed821
1687859200 <1687859200@qq.com>
2024-03-07 23:01:11 +0800

    2024-3-7-23

---
+++ src/main/java/com/w36/state/KeyedListStateDemo.java
@@ -0,0 +1,56 @@
+package com.w36.state;
+
+import com.w36.bean.WaterSensor;
+import com.w36.function.WaterSensorMapFunction;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.util.Collector;
+
+import java.time.Duration;
+
+public class KeyedVauleStateDemo {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        SingleOutputStreamOperator<WaterSensor> sensorDS = env
+                .socketTextStream("106.15.42.75", 7777)
+                .map(new WaterSensorMapFunction());
+        WatermarkStrategy<WaterSensor> watermarkStrategy = WatermarkStrategy
+                .<WaterSensor>forBoundedOutOfOrderness(Duration.ofSeconds(3))
+                .withTimestampAssigner((element, recordTimestamp) -> element.getTs() * 1000L);
+
+        sensorDS.keyBy(r -> r.getId())
+                .process(
+                        new KeyedProcessFunction<String, WaterSensor, String>() {
+
+                            ValueState<Integer> lastVcState;
+
+                            @Override
+                            public void open(Configuration parameters) throws Exception {
+                                super.open(parameters);
+                                // 状态描述器
+                                lastVcState = getRuntimeContext().getState(new ValueStateDescriptor<Integer>("lastVcState", Types.INT));
+                            }
+
+                            @Override
+                            public void processElement(WaterSensor waterSensor, KeyedProcessFunction<String, WaterSensor, String>.Context context, Collector<String> collector) throws Exception {
+                                int lastVc = lastVcState.value() == null ? 0 : lastVcState.value();
+                                Integer vc = waterSensor.getVc();
+                                if (Math.abs(vc - lastVc) > 10) {
+                                    collector.collect("NOW：" + vc + ",LAST：" + lastVc);
+                                }
+                                lastVcState.update(vc);
+                            }
+                        }
+                )
+                .print();
+
+        env.execute();
+    }
+}
